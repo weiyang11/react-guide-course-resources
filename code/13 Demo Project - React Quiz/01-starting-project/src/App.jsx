9649705b4ebf3f0193123342939bbbd36e3dca3d
weiyang <weiyang_11@outlook.com>
2024-07-28 16:20:31 +0800

    quiz and header component created

--- code/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
@@ -1,3 +1,10 @@
-function App() {}
+import Header from './components/Header';
+
+
+function App() {
+  return (
+  <Header />
+  )
+}
 
 export default App;
---
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+
+export default function Quiz(){
+
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  return(
+    <p>Currently Active Question</p>
+
+  )
+};
