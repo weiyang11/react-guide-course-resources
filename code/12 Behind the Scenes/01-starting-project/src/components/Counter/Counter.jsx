4f9d9088878f8d37d7d4b16cd3df7d86a6556017
weiyang <weiyang_11@outlook.com>
2024-07-29 18:28:05 +0800

    key matter when manage state

--- code/12 Behind the Scenes/01-starting-project/src/components/Counter/Counter.jsx
+++ code/12 Behind the Scenes/01-starting-project/src/components/Counter/Counter.jsx
@@ -5,6 +5,7 @@ import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 function isPrime(number) {
   log(
@@ -32,14 +33,22 @@ const Counter =  memo(function Counter({ initialCount }) {
 
   const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+  // const [counter, setCounter] = useState(initialCount);
+
+  const [counterChanges, setCounterChanges] = useState([{value:initialCount, id: Math.random() * 1000}]);
+
+  const currentCounter = counterChanges.reduce(
+    (preCounter, counterChange) => preCounter + counterChange.value, 0
+  );
 
   const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{value: -1, id: Math.random() * 1000 }, ...prevCounterChanges]);
   }, [])
 
   const handleIncrement = useCallback (function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [{value: 1, id: Math.random() * 1000 }, ...prevCounterChanges]);
   }, [])
 
   return (
@@ -52,11 +61,12 @@ const Counter =  memo(function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges}/>
     </section>
   );
 })
