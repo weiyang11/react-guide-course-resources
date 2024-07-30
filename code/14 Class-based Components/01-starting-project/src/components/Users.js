6381e5fc939f78c90032ca236b561b99dd04e93a
weiyang <weiyang_11@outlook.com>
2024-07-30 16:35:52 +0800

    add the user finder component and it's css for the filter function

--- code/14 Class-based Components/01-starting-project/src/components/Users.js
+++ code/14 Class-based Components/01-starting-project/src/components/Users.js
@@ -23,7 +23,7 @@ class Users extends Component {
   render(){
     const usersList = (
       <ul>
-        {DUMMY_USERS.map((user) => (
+        {this.props.users.map((user) => (
           <User key={user.id} name={user.name} />
         ))}
       </ul>
