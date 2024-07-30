49b6df51a164400a6be46661077dbbb8d1f76527
weiyang <weiyang_11@outlook.com>
2024-07-30 17:58:27 +0800

    error boundary

--- code/14 Class-based Components/01-starting-project/src/components/Users.js
+++ code/14 Class-based Components/01-starting-project/src/components/Users.js
@@ -16,6 +16,19 @@ class Users extends Component {
       showUsers: true
     };
   }
+
+  componentDidUpdate(){
+    // try {
+    //   someCodeWhichMightFail()
+  // }
+    //   catch(err){
+    //     handle error
+    //   }
+    if(this.props.users.length === 0){
+      throw new Error('No users provided!');
+    }
+  }
+
   toggleUsersHandler(){
     this.setState((curState) => {return {showUsers: !curState.showUsers}});
   }
