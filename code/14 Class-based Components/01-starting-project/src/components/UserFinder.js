ff1bc446d98bbd08d153c53fe95869ac1789a74d
weiyang <weiyang_11@outlook.com>
2024-07-30 17:40:54 +0800

    add usecontext for classbase function

--- code/14 Class-based Components/01-starting-project/src/components/UserFinder.js
+++ code/14 Class-based Components/01-starting-project/src/components/UserFinder.js
@@ -2,6 +2,7 @@ import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
+import UsersContext from '../store/users-context';
 
 const DUMMY_USERS = [
   { id: 'u1', name: 'Max' },
@@ -10,6 +11,8 @@ const DUMMY_USERS = [
 ];
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor(){
     super();
     this.state = {
@@ -19,13 +22,13 @@ class UserFinder extends Component {
   }
 
   componentDidMount(){
-    this.setState({filteredUsers: DUMMY_USERS});
-  } 
+    this.setState({filteredUsers: this.context.users});
+  }
 
   componentDidUpdate(prevProps, prevState){
     if(prevState.searchTerm !== this.state.searchTerm){
       this.setState({
-        filteredUsers:DUMMY_USERS.filter((user) =>
+        filteredUsers:this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
       )});
     }
