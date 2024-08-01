47fa0c099b763db724b9975df710ea8a3fe1570d
weiyang <weiyang_11@outlook.com>
2024-08-01 17:40:15 +0800

    add http js to use asyn and await for fetching the place data

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import {useState, useEffect} from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx'
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 
 export default function AvailablePlaces({ onSelectPlace }) {
@@ -14,16 +15,12 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/places')
-        const resData = await response.json()
 
-        if(!response.ok){
-          throw new Error ('Fail to fetch places')
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition((position) => {
           console.log(position);
-          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude)
+          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude)
           setAvailablePlaces(sortedPlaces)
           setIsFetching(false);
         })
