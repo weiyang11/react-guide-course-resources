e6d51428c7b28da09c76bcdfa580662845b5504c
weiyang <weiyang_11@outlook.com>
2024-08-01 16:40:25 +0800

    use asyn and await for fetching data

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -6,10 +6,16 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/places')
-    .then((response) =>{ return response.json()})
-    .then((resData) => {setAvailablePlaces(resData.places)});
+  useEffect( () => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places')
+      const resData = await response.json()
+      setAvailablePlaces(resData.places)
+    }
+    // fetch('http://localhost:3000/places')
+    // .then((response) =>{ return response.json()})
+    // .then((resData) => {setAvailablePlaces(resData.places)});
+    fetchPlaces();
   },[])
 
   return (
