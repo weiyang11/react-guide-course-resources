43b8e7b83080d5834e29580d3ab10b973d4670cc
weiyang <weiyang_11@outlook.com>
2024-08-01 17:31:17 +0800

    transform fetching data

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx'
+import { sortPlacesByDistance } from '../loc.js';
 
 
 export default function AvailablePlaces({ onSelectPlace }) {
@@ -19,12 +20,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if(!response.ok){
           throw new Error ('Fail to fetch places')
         }
-        setAvailablePlaces(resData.places)
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log(position);
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude)
+          setAvailablePlaces(sortedPlaces)
+          setIsFetching(false);
+        })
+
       } catch (error) {
         setError({message: error.message || "Could not fetch places, please try again later"})
+        setIsFetching(false);
       }
 
-      setIsFetching(false);
     }
     // fetch('http://localhost:3000/places')
     // .then((response) =>{ return response.json()})
--- code/15 HTTP Requests/01-starting-project/src/components/Places.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/Places.jsx
@@ -1,6 +1,6 @@
 export default function Places({ title, places, fallbackText, onSelectPlace, isLoading, loadingText }) {
-  console.log(places);
-  console.log(places.length);
+  // console.log(places);
+  // console.log(places.length);
   return (
     <section className="places-category">
       <h2>{title}</h2>
