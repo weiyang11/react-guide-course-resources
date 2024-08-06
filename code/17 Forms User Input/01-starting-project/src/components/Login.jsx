359a3bc8648ab58861623fa9cc06bc03d2ccf97b
weiyang <weiyang_11@outlook.com>
2024-08-06 17:49:04 +0800

    managing and getting user input via state and generic handlers

--- code/17 Forms User Input/01-starting-project/src/components/Login.jsx
+++ code/17 Forms User Input/01-starting-project/src/components/Login.jsx
@@ -1,23 +1,51 @@
+import {useState} from 'react';
+
 export default function Login() {
+
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  const [enterValues, setEnterValues] = useState({
+    email: '',
+    password: ''
+  });
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+
+  }
+
+  function handleEmailChange(e){
+    setEmail(e.target.value);
+  }
+
+  function handleInputChange(identifier, value){
+    setEnterValues(prevValues => ({
+      ...prevValues,
+      [identifier]: value
+    }))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email"  onChange={(event)=> handleInputChange('email', event.target.value)}  value={enterValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event)=> handleInputChange('password', event.target.value)}  value={enterValues.password} />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button  className="button">Login</button>
       </p>
     </form>
   );
