a2f1e0eb42850e565a9877c6207bd066e491c241
weiyang <weiyang_11@outlook.com>
2024-08-08 17:24:23 +0800

    install redux toolkit, and adding the state slice

--- code/19 Redux Basics/01-starting-project/src/store/index.js
+++ code/19 Redux Basics/01-starting-project/src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
+
 
 const initailState = {counter: 0, showCounter: true};
+
+createSlice({
+  name: 'counter',
+  initialState: initailState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
 
 const counterReducer = (state = initailState, action) => {
   if (action.type === 'increment') {
