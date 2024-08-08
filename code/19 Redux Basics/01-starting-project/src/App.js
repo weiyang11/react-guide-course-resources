54525d6a76a9282a26b0695700c3cce5d8cbc225
weiyang <weiyang_11@outlook.com>
2024-08-08 18:22:47 +0800

    reading and dispatching from new slice

--- code/19 Redux Basics/01-starting-project/src/App.js
+++ code/19 Redux Basics/01-starting-project/src/App.js
@@ -1,13 +1,17 @@
 import Counter from './components/Counter';
 import Auth from './components/Auth';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import { useSelector } from 'react-redux';
 
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
   return (
     <>
     <Header />
-    <Auth />
+    {!isAuth && <Auth />}
+    {isAuth && <UserProfile />}
     <Counter />
     </>
   );
