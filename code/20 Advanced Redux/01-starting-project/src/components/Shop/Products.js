7c7f4a319ed06a019609c8373be2ddd98d2dfac8
weiyang <weiyang_11@outlook.com>
2024-08-09 16:13:41 +0800

    redux basic refresh practice

--- code/20 Advanced Redux/01-starting-project/src/components/Shop/Products.js
+++ code/20 Advanced Redux/01-starting-project/src/components/Shop/Products.js
@@ -1,16 +1,37 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+
 const Products = (props) => {
+
+  const DUMMY_PRODUCT = [{
+    id: 'p1',
+    price: 6,
+    title: "My First Book",
+    description: "The first book i ever wrote"
+  },
+  {
+    id: 'p2',
+    price: 4,
+    title: "My Second Book",
+    description: "The second book i ever wrote"
+  }
+
+]
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCT.map(product => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
