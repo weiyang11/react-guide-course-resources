7cbc587d7b49a9c6f69b34f06a6a5c95978ca037
weiyang <weiyang_11@outlook.com>
2024-08-10 16:50:14 +0800

    alternative way of define routes by jsx based (createRoutesFromElements and Route)

--- code/21 Routing/01-starting-project/src/App.js
+++ code/21 Routing/01-starting-project/src/App.js
@@ -1,6 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Product";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
   {path: '/', element: <HomePage />},
