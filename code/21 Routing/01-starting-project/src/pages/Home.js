8769ed948acc067d805720f08c8d024de31237de
weiyang <weiyang_11@outlook.com>
2024-08-10 16:56:29 +0800

    navigating between pages with link

--- code/21 Routing/01-starting-project/src/pages/Home.js
+++ code/21 Routing/01-starting-project/src/pages/Home.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>
+        Go to <Link to='/products'>the list of products</Link>
+      </p>
     </div>
   );
 }
--- code/21 Routing/01-starting-project/src/pages/Product.js
+++ code/21 Routing/01-starting-project/src/pages/Product.js
@@ -2,6 +2,7 @@ function ProductsPage() {
   return (
     <div>
       <h1>Product Page</h1>
+    
     </div>
   );
 }
