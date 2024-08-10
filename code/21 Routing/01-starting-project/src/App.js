a72aee6ef563a53640cc5f74992a2b69a5cd48c5
weiyang <weiyang_11@outlook.com>
2024-08-10 17:12:32 +0800

    layout and nexted routes

--- code/21 Routing/01-starting-project/src/App.js
+++ code/21 Routing/01-starting-project/src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Product";
+import RootLayout from "./pages/Root";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -12,8 +13,12 @@ import ProductsPage from "./pages/Product";
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage />},
-  {path: '/products', element: <ProductsPage />},
+  { path: '/',
+    element: <RootLayout /> ,
+    children:[
+    {path: '/', element: <HomePage />},
+    {path: '/products', element: <ProductsPage />},
+  ]},
 ])
 
 function App() {
