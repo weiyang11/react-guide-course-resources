f380937f62555f44345959cd33366d41af6ccff2
weiyang <weiyang_11@outlook.com>
2024-08-10 19:13:47 +0800

    add params for event detail page

--- code/21 Routing/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage(){
+  const params = useParams();
+
   return (
     <div>
       <h1>Event Detail Page</h1>
+      <p>Event ID: {params.eventId}</p>
     </div>
   );
 }
