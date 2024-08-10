d3a36dd9bf7fb1f797d26e3f0ad7ef3dd31a01af
weiyang <weiyang_11@outlook.com>
2024-08-10 16:46:03 +0800

    adding second route

--- code/21 Routing/01-starting-project/src/App.js
+++ code/21 Routing/01-starting-project/src/App.js
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Product";
 
 const router = createBrowserRouter([
   {path: '/', element: <HomePage />},
-  {}
+  {path: '/products', element: <ProductsPage />},
 ])
 
 function App() {
---
+++ code/21 Routing/01-starting-project/src/pages/Product.js
@@ -0,0 +1,10 @@
+function ProductsPage() {
+  return (
+    <div>
+      <h1>Product Page</h1>
+    </div>
+  );
+}
+
+
+export default ProductsPage;
