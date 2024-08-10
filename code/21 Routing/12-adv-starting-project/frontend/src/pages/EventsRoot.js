9f8274ab8333d141896d1fad319ace8cff1b8e55
weiyang <weiyang_11@outlook.com>
2024-08-10 19:19:29 +0800

    add eventNavigation to wrap all event related page

---
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/EventsRoot.js
@@ -0,0 +1,14 @@
+import EventsNavigation from "../components/EventsNavigation";
+import { Outlet } from "react-router-dom";
+
+function EventsRootLayout() {
+  return (
+    <div>
+      <EventsNavigation />
+      <Outlet />
+    </div>
+  );
+}
+
+
+export default EventsRootLayout;
