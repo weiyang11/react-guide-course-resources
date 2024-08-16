a9c110b8154a14305b0965dac1abd0fc396b2324
weiyang <weiyang_11@outlook.com>
2024-08-16 17:52:06 +0800

    defer data fetching with defer()

--- code/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,25 +1,32 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { json, useLoaderData,defer, Await } from 'react-router-dom';
+import { Suspense } from 'react';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
 
-  const data = useLoaderData();
+  const {events} = useLoaderData();
   // if (data.isError) {
   //   return <p>{data.message}</p>;
   // }
-  const events = data.events;
+  // const events = data.events;
 
-  return (
-    <>
-    <EventsList events={events} />
-    </>
-  );
+  // return (
+  //   <>
+  //   <EventsList events={events} />
+  //   </>
+  // );
+  return(
+  <Suspense fallback={<p style={{textAlign: 'center'}}>Loading</p>}>
+    <Await resolve={events}>
+      {(loadedEvents)=> <EventsList events={loadedEvents} />}
+    </Await>
+  </Suspense>)
 }
 
 export default EventsPage;
 
-export async function loader(){
+async function loadEvents(){
   const response = await fetch('http://localhost:8080/events');
   if (!response.ok) {
     // return {isError: true, message: 'Something went wrong!'};
@@ -30,6 +37,13 @@ export async function loader(){
     // const resData = await response.json();
     // // const res = new Response('any data', {state: 20});
     // return resData.events;
-    return response;
+    const resData= await response.json();
+    return resData.events;
   }
+}
+
+export function loader(){
+  return defer({
+    events: loadEvents()
+  })
 }
