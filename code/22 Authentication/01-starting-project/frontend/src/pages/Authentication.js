8be213d1efee03d91e2c7342fa48da139af2d722
weiyang <weiyang_11@outlook.com>
2024-08-18 19:24:32 +0800

    attaching auth token to outgoing requests

--- code/22 Authentication/01-starting-project/frontend/src/pages/Authentication.js
+++ code/22 Authentication/01-starting-project/frontend/src/pages/Authentication.js
@@ -39,5 +39,10 @@ export async function action({ request }) {
   }
 
   // soon: manage that token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem('token', token);
+  
   return redirect('/');
 }
