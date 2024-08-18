bf2d8a31035a840d06b791a4a523266320727a42
weiyang <weiyang_11@outlook.com>
2024-08-18 20:10:28 +0800

    managing the token expiration

--- code/22 Authentication/01-starting-project/frontend/src/pages/Root.js
+++ code/22 Authentication/01-starting-project/frontend/src/pages/Root.js
@@ -2,6 +2,7 @@ import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
   const token = useLoaderData();
@@ -11,9 +12,17 @@ function RootLayout() {
     if(!token){
       return;
     }
+
+    if(token === 'EXPIRED'){
+      submit(null, {action: '/logout', method: 'post'});
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
     setTimeout(()=> {
       submit(null, {action: '/logout', method: 'post'});
-    }, 1* 60 * 60 *1000);
+    }, tokenDuration);
   },[token, submit]);
 
   return (
