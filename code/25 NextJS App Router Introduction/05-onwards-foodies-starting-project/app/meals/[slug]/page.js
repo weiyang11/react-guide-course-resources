c080a8e10286f82093cfae6ffc19e86dfa566226
weiyang <weiyang_11@outlook.com>
2024-08-22 21:48:39 +0800

    created the fix and dynamic routes

---
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailPage({params}){
+  return (
+    <div>
+      <h1>{params.slug}</h1>
+    </div>
+  );
+}
---
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMealPage() {
+  return (
+    <div>
+      <h1>Share Meal</h1>
+    </div>
+  );
+}
