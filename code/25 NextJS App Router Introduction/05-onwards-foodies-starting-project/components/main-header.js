ea839309c0c52f97feaa019ba1aded1b79a23164
weiyang <weiyang_11@outlook.com>
2024-08-22 21:59:36 +0800

    Adding a Custom Components to layout

---
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import logoImg from '@/assets/logo.png';
+
+export default function MainHeader() {
+  return (
+    <header>
+      <Link href='/'>
+        <img src={logoImg.src} alt='NextLevel Food' />
+        NextLevel Food
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/meals'>Browser Meals</Link>
+          </li>
+          <li>
+            <Link href='/community'>Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
