20eaa749429c964b6a3ce185be230f156e31373f
weiyang <weiyang_11@outlook.com>
2024-08-22 22:15:57 +0800

    optimizing images with the NextJS image component

--- code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 
@@ -6,7 +7,7 @@ export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href='/'>
-        <img src={logoImg.src} alt='NextLevel Food' />
+        <Image src={logoImg} alt='NextLevel Food' priority />
         NextLevel Food
       </Link>
       <nav className={classes.nav}>
