52aade954f2dc5a7d2d4db6b6053a0555d0eb836
weiyang <weiyang_11@outlook.com>
2024-08-22 22:24:11 +0800

    using more custom components

--- code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
@@ -2,9 +2,12 @@ import Link from 'next/link';
 import Image from 'next/image';
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
+import MainHeaderBackground from './main-header-background';
 
 export default function MainHeader() {
   return (
+    <>
+    <MainHeaderBackground />
     <header className={classes.header}>
       <Link className={classes.logo} href='/'>
         <Image src={logoImg} alt='NextLevel Food' priority />
@@ -21,5 +24,6 @@ export default function MainHeader() {
         </ul>
       </nav>
     </header>
+    </>
   );
 }
