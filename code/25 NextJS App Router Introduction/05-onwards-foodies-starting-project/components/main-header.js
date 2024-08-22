4c6dfab8dc6016b127e8201b1bda36bede1aa8df
weiyang <weiyang_11@outlook.com>
2024-08-22 22:09:45 +0800

    styling NextJS project using CSS Modules

--- code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
@@ -1,14 +1,15 @@
 import Link from 'next/link';
 import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href='/'>
+    <header className={classes.header}>
+      <Link className={classes.logo} href='/'>
         <img src={logoImg.src} alt='NextLevel Food' />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href='/meals'>Browser Meals</Link>
