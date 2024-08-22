3e11413b2e6228509c2c64f1c19313a63ceb80c7
weiyang <weiyang_11@outlook.com>
2024-08-22 16:34:33 +0800

    using the query key as query function input

--- code/24 React Query/01-starting-project/src/util/http.js
+++ code/24 React Query/01-starting-project/src/util/http.js
@@ -2,12 +2,16 @@ import { QueryClient } from '@tanstack/react-query';
 
 export const queryClient = new QueryClient();
 
-export async function fetchEvents({ signal, searchTerm }) {
-  console.log(searchTerm);
+export async function fetchEvents({ signal, searchTerm, max }) {
+
   let url = 'http://localhost:3000/events';
 
-  if (searchTerm) {
+  if (max&&searchTerm) {
+    url += '?search=' + searchTerm + '&max=' + max;
+  }else if (searchTerm) {
     url += '?search=' + searchTerm;
+  }else if (max){
+    url += '?max=' + max;
   }
 
   const response = await fetch(url, { signal: signal });
