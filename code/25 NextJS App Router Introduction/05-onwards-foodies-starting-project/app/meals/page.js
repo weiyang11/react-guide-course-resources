e0d5e60dd5ccd99473166f37002eae698caea864
weiyang <weiyang_11@outlook.com>
2024-08-24 18:02:42 +0800

    Using Suspense & Streamed Responses for granular loading state management

--- code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/meals/page.js
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/meals/page.js
@@ -2,9 +2,16 @@ import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function MealsPage() {
+async function Meals(){
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />
+}
+
+export default function MealsPage() {
+
 
   return (
     <>
@@ -18,7 +25,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching Meals ....</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
