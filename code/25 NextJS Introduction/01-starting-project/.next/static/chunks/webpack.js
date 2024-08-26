1e5a5a6f2b2a550ec76d897c4e1c827b9d904611
weiyang <weiyang_11@outlook.com>
2024-08-26 16:52:18 +0800

    adding nested parts and pages

--- code/25 NextJS Introduction/01-starting-project/.next/static/chunks/webpack.js
+++ code/25 NextJS Introduction/01-starting-project/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dfe8d8a4070a3fa8"; }
+/******/ 		__webpack_require__.h = function() { return "1feee004eb7758b8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
