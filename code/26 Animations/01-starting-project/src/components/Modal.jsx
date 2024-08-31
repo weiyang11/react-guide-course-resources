8d348f898b521e421f4d928035206d13ade1aea5
weiyang <weiyang_11@outlook.com>
2024-08-31 16:15:31 +0800

    nexted animation and variants

--- code/26 Animations/01-starting-project/src/components/Modal.jsx
+++ code/26 Animations/01-starting-project/src/components/Modal.jsx
@@ -8,7 +8,8 @@ export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <motion.dialog variants={{hidden: {opacity: 0, y: 30}, visible:{opacity: 1, y: 0}}} initial='hidden' animate='visible' exit='hidden' open className="modal">
+      <motion.dialog variants={{hidden: {opacity: 0, y: 30}, visible:{opacity: 1, y: 0}}}
+      initial='hidden' animate='visible' exit='hidden' open className="modal">
         <h2>{title}</h2>
         {children}
       </motion.dialog>
