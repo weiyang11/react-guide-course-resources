df791be80d7e05864bb291c810cc3200a4819fe4
weiyang <weiyang_11@outlook.com>
2024-09-02 17:46:14 +0800

    using the context API

--- code/27 Replacing Redux with Hooks/01-starting-project/src/containers/Products.js
+++ code/27 Replacing Redux with Hooks/01-starting-project/src/containers/Products.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React,{useContext} from 'react';
+// import { useSelector } from 'react-redux';
 
 import ProductItem from '../components/Products/ProductItem';
+import { ProductContext } from '../context/product-context';
 import './Products.css';
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  // const productList = useSelector(state => state.shop.products);
+
+  const productList = useContext(ProductContext).products;
   return (
     <ul className="products-list">
       {productList.map(prod => (
--- code/27 Replacing Redux with Hooks/01-starting-project/src/index.js
+++ code/27 Replacing Redux with Hooks/01-starting-project/src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+// import productReducer from './store/reducers/products';
+import ProductsProvider from './context/product-context';
 
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
+// const rootReducer = combineReducers({
+//   shop: productReducer,
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <ProductsProvider >
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
