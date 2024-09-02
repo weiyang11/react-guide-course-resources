baeb71bc3818edfe409988b1476871b8fb4780d8
weiyang <weiyang_11@outlook.com>
2024-09-02 18:12:05 +0800

    create the custom hook

---
+++ code/27 Replacing Redux with Hooks/01-starting-project/src/hooks-store/store.js
@@ -0,0 +1,18 @@
+import {useState, useEffect} from 'react';
+
+let globalState ={};
+let listeners = [];
+let actions = {};
+
+const useStore =() => {
+    const setState = useState(globalState)[1];
+
+    useEffect(() => {
+        listeners.push(setState);
+
+        return () => {
+            listeners = listeners.filter(li => li !== setState); // remove the listener
+        }
+    }, [setState]); // [] means it will only run once
+
+}
