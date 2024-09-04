3ac7525b3d4989a003443188e70cc57727f7adbd
weiyang <weiyang_11@outlook.com>
2024-09-04 17:59:25 +0800

    use mock for async testing

--- code/28 Testing/01-starting-project/src/components/Async.test.js
+++ code/28 Testing/01-starting-project/src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from './Async';
 
 describe('Async component',()=>{
   test('renders posts if request is successful', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First Post' }],
+    });
     render(<Async />);
     const listItemElements = await screen.findAllByRole('listitem');
     expect(listItemElements).not.toHaveLength(0);
